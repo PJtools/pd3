410657de9fdc77a0b82218efd029365a283282ec
PJtools <pjtools@vip.qq.com>
2017-02-21 09:42:56 +0800

    Initialization pd3.

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const webpack = require('webpack');
+const path = require('path');
+const PROD = process.argv.indexOf('-p') >= 0;
+
+const config = {
+  // ----------------------------------
+  // Project Structure
+  // ----------------------------------
+  path_base  : path.resolve(__dirname, ''),
+  dir_client : 'src',
+  dir_dist   : 'dist',
+  dir_node_modules : 'node_modules',
+
+  // ----------------------------------
+  // Compiler Configuration
+  // ----------------------------------
+  compiler_babel : {
+    cacheDirectory : true,
+    plugins        : ['transform-runtime'],
+    presets        : ['es2015', 'react', 'stage-0']
+  },
+  compiler_devtool         : 'source-map'
+};
+
+// ------------------------------------
+// Utilities
+// ------------------------------------
+function base () {
+  const args = [config.path_base].concat([].slice.call(arguments));
+  return path.resolve.apply(path, args)
+}
+
+config.utils_paths = {
+  base   : base,
+  client : base.bind(null, config.dir_client),
+  dist   : base.bind(null, config.dir_dist),
+  node_modules : base.bind(null, config.dir_node_modules)
+};
+
+const paths = config.utils_paths;
+
+// ------------------------------------
+// Entry Points
+// ------------------------------------
+const webpackConfig = {
+  name: 'client',
+  target: 'web',
+  devtool: config.compiler_devtool,
+  resolve: {
+    root: paths.client(),
+    extensions: ['', '.js', '.jsx', '.json'],
+    alias: {
+      'd3': 'd3'
+    }
+  },
+  externals: {
+    'd3': 'd3'
+  },
+  module: {
+    noParse: []
+  }
+};
+
+// ------------------------------------
+// Entry Points
+// ------------------------------------
+webpackConfig.entry = paths.client('index.js');
+
+// ------------------------------------
+// Bundle Output
+// ------------------------------------
+webpackConfig.output = {
+  libraryTarget: 'umd',
+  path: paths.dist(),
+  filename: PROD ? `pd3.min.js` : `pd3.js`,
+};
+
+// ------------------------------------
+// Plugins
+// ------------------------------------
+if (PROD) {
+  webpackConfig.plugins = [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { unused: true, dead_code: true, warnings: false }
+    })
+  ];
+}
+else {
+  webpackConfig.plugins = [];
+}
+
+// ------------------------------------
+// Loaders
+// ------------------------------------
+// JavaScript / JSON
+webpackConfig.module.loaders = [{
+  test: /\.(js|jsx)$/,
+  exclude: /node_modules/,
+  loader: 'babel',
+  query: config.compiler_babel
+}, {
+  test: /\.json$/,
+  loader: 'json'
+}];
+
+module.exports = webpackConfig;
